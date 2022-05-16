19f311785949e08a825a6343d3188c0708961602
Ruby Lin <rubylin@Rubys-MacBook-Pro.local>
2022-05-16 15:56:55 -0700

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -1,3 +1,13 @@
 """
 Load face image dataset
 """
+import numpy as np
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torchvision.transforms.functional as TF
+
+
+def fetch_dataloader(data_dir, batch_size, workers):
+    # TODO
+    pass
