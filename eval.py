a5507cfec0a4a30b1a281aca6d02bf9975a93248
Ruby Lin <rubylin@Rubys-MacBook-Pro.local>
2022-05-16 15:29:32 -0700

    Update eval.py and still working on it

--- eval.py
+++ eval.py
@@ -8,15 +8,16 @@ import os
 import numpy as np
 import torch
 
+import utils
 import model
 import dataloader
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--data_dir", default = "data/test",
+parser.add_argument("--data_dir", default = "data/val",
                     help = "Directory containing the dataset")
 parser.add_argument("--model_dir", default = "",
                     help = "Directory saving the model and log files")
-parser.add_argument("--pretrained", default = "best",
+parser.add_argument("--pretrained", default = "None",
                     help = "Optional, filename in --model_dir containing weights to load")  # 'best' or 'train'
 
 
@@ -33,3 +34,15 @@ if __name__ == '__main__':
     """
         Evaluate the model on the test set
     """
+    args = parser.parse_args()
+
+    # Define model
+    identities = 1506
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net = model.FocusFace(identities).to(device)
+    # Load weights from the saved file
+    if args.pretrained is not None:
+        pretrain_path = os.path.join(
+            args.model_dir, args.pretrained + ".pth.tar")
+        logging.info("Loading parameters from {}".format(pretrain_path))
+        utils.load_checkpoint(pretrain_path, net)
