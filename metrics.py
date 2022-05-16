78d2ca68de5c0e35ec12cb1ec29e5bd9c18f7f54
Ruby Lin <rubylin@Rubys-MacBook-Pro.local>
2022-05-15 23:57:01 -0700

    Import pyeer

--- metrics.py
+++ metrics.py
@@ -1,3 +1,6 @@
 """
 Compute evaluation metrics
 """
+from pyeer.eer_info import get_eer_stats
+from pyeer.report import generate_eer_report, export_error_rates
+from pyeer.plot import plot_eer_stats
