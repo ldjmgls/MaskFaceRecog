9198f17b2a154e3a0a2b97b6e088fbbf996d8b9e
Sree Prasanna Rajagopal <sreer@uw.edu>
2022-05-18 09:33:44 -0700

    Structuring

--- dataloader.py
+++ dataloader.py
@@ -8,6 +8,20 @@ import torchvision.transforms as transforms
 import torchvision.transforms.functional as TF
 
 
-def fetch_dataloader(data_dir, batch_size, workers):
-    # TODO
+def mask_on_dataloader(dataloader, percentage):
+    """ The idea here is to mask a `percentage` of the faces in the loader with a mask
+        Here we use `aqeelanwar/MaskTheFace` to create these synthetic images
+        
+        Returns a new dataloader which contains the masked faces
+    """ 
+    pass
+
+
+def create_dataloader(data_dir, batch_size, workers):
+    """ The folder is organized as follows: a trainset called `train`, a validation set called `val`
+        Each of them have numbered folders
+        
+        This method recurses through each of the folders to create two dataloaders: one for train and one for validation. 
+        Returns two dataloaders, one train, one validation
+    """
     pass
