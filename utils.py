42b046e7716ff8eae0ce5600a27138411962cf8c
Ruby Lin <rubylin@Rubys-MacBook-Pro.local>
2022-05-31 13:07:19 -0700

    Add save_dict_to_json()

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import sys
@@ -31,6 +32,17 @@ def set_logger(log_path):
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setFormatter(logging.Formatter("%(message)s"))
         logger.addHandler(stream_handler)
+
+def save_dict_to_json(d, json_path):
+    """Saves dict of floats in json file
+    Args:
+        d: (dict) of float-castable values (np.float, int, float, etc.)
+        json_path: (string) path to json file
+    """
+    with open(json_path, 'w') as f:
+        # We need to convert the values to float for json (it doesn't accept np.array, np.float, )
+        d = {k: float(v) for k, v in d.items()}
+        json.dump(d, f, indent=4)
 
 def save_checkpoint(state, is_best, checkpoint):
     """
